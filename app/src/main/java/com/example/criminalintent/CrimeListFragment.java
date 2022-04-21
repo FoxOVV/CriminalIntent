d6d76bd7977390d16b5970209cf363b5309188be
FoxOVV <Osipov.vadim445@mail.ru>
2022-04-21 20:29:35 +0500

    21.04 Добавил комментарии

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -32,7 +33,7 @@ public class CrimeListFragment extends Fragment {
         updateUI();
         return view;
     }
-
+    //Создает объект CrimeAdapter и назначает его RecyclerView
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrime();
@@ -40,12 +41,28 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
+    //Как вягледет ViewHolder -> itemView -> View (это область на экране в которую помещаются все 11 преступлений)
+    //Создан чтобы удерживает объект View
     private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private Crime mCrime;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
         }
     }
 
+    //Активируется, когда требуется создать новый объект ViewHolder
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
         private List<Crime> mCrimes;
 
@@ -55,15 +72,18 @@ public class CrimeListFragment extends Fragment {
 
         @NonNull
         @Override
-        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Вызывается, когда ему требуется новое представление для отображения элемента
+        //Другими словами вызывается 11 раз - столько записей одновременно помещается на экран смартфона
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             Log.i(TAG,"TAGonCreateViewHolder");
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             return new CrimeHolder(layoutInflater, parent);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
-
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime =  mCrimes.get(position);
+            holder.bind(crime);
         }
 
         @Override
